3ec01f9ce9b8d7010217c570d2e354d4290aa099
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-16 22:51:10 -0300

    Update main.tf

--- terraform/modules/service-networking/main.tf
+++ terraform/modules/service-networking/main.tf
@@ -1,1 +1,17 @@
-#
+resource "azurerm_express_route_circuit" "example" {
+  name                  = "expressRoute1"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  service_provider_name = "Equinix"
+  peering_location      = "Silicon Valley"
+  bandwidth_in_mbps     = 50
+
+  sku {
+    tier   = "Standard"
+    family = "MeteredData"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
