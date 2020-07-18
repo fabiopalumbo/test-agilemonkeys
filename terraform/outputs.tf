8c1636a6d01766ff822189930491bdbe1ecb112e
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-18 15:20:41 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "bastion_public_ip" {
   value = module.bastion.public_ip_address
 }
 output "app_ip" {
-          value = module.jenkins.network_interface_private_ip
+          value = module.app.network_interface_private_ip
 }
 output "db_ip" {
-          value = module.chef.network_interface_private_ip
+          value = module.db.network_interface_private_ip
 }
 
