f052026e046708edcc5542b51659669a7fd2f98d
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-18 15:26:13 -0300

    Update main.tf

--- terraform/modules/alerts/main.tf
+++ terraform/modules/alerts/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "to_monitor" {
   name                     = "Alert_Storage"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
