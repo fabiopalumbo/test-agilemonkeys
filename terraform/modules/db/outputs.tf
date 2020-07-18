ec72817c48621e696d3845d51ff1839c14b6fbbe
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-18 15:34:47 -0300

    Update outputs.tf

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -1,26 +1,26 @@
 
 output "database_name" {
-  value       = azurerm_sql_database.db.name
+  value       = azurerm_sql_database.devops.name
 }
 
 output "sql_server_name" {
-  value       = azurerm_sql_server.server.name
+  value       = azurerm_sql_server.devops.name
 }
 
 output "sql_server_location" {
-  value       = azurerm_sql_server.server.location
+  value       = azurerm_sql_server.devops.location
 }
 
 output "sql_server_version" {
-  value       = azurerm_sql_server.server.version
+  value       = azurerm_sql_server.devops.version
 }
 
 output "sql_server_fqdn" {
-  value       = azurerm_sql_server.server.fully_qualified_domain_name
+  value       = azurerm_sql_server.devops.fully_qualified_domain_name
 }
 
 output "network_interface_private_ip" {
-  value       = azurerm_sql_server.server.network_interface_private_ip
+  value       = azurerm_sql_server.devops.network_interface_private_ip
 }
 
 
