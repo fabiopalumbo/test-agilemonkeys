1ff060cfcf7c17c089035a06a88c12a41df53ad5
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-18 15:14:41 -0300

    Update main.tf

--- terraform/modules/backup/main.tf
+++ terraform/modules/backup/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_recovery_services_vault" "devops" {
   name                = "${var.project}-vault"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
   sku                 = "Standard"
 }
 
 resource "azurerm_recovery_services_protection_policy_vm" "devops" {
   name                = "${var.project}-vault-policy"
-  resource_group_name = "${var.resource_group_name}"
-  recovery_vault_name = "${azurerm_recovery_services_vault.devops.name}"
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.devops.name
 
   backup {
-    frequency = "${var.backup_frequency}"
-    time      = "${var.backup_time}"
+    frequency = var.backup_frequency
+    time      = var.backup_time
   }
  
   retention_daily {
@@ -21,8 +21,8 @@ resource "azurerm_recovery_services_protection_policy_vm" "devops" {
 }
 
 resource "azurerm_recovery_services_protected_vm" "vm1" {
-  resource_group_name = "${var.resource_group_name}"
-  recovery_vault_name = "${azurerm_recovery_services_vault.devops.name}"
-  source_vm_id        = "${var.vm_id}"
-  backup_policy_id    = "${azurerm_recovery_services_protection_policy_vm.devops.id}"
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.devops.name
+  source_vm_id        = var.vm_id
+  backup_policy_id    = azurerm_recovery_services_protection_policy_vm.devops.id
 }
